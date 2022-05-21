14c0fba55d702719a3348914c4c2025eedc96c13
ginpei <ginpei@gmail.com>
2022-05-21 19:57:09 +0000

    docs: ✏️ improve types

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,9 @@
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const markdownIt = require("markdown-it");
 
+/**
+ * @param {import("@11ty/eleventy/src/UserConfig")} eleventyConfig
+ */
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy({ static: "/" });
 
